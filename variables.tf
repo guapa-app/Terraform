99ec6124a38707ddddc34fd59d8c3d4ef20fbbe0
Ahmed Aamer <el3ashe2@gmail.com>
2025-10-19 12:35:24 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,32 @@
+variable "tenancy_ocid" {
+  description = "OCI tenancy OCID"
+  type        = string
+}
+
+variable "compartment_ocid" {
+  description = "OCI compartment OCID"
+  type        = string
+}
+
+variable "user_ocid" {
+  description = "OCI user OCID"
+  type        = string
+}
+
+variable "fingerprint" {
+  description = "API key fingerprint"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to your OCI API private key"
+  type        = string
+}
+
+variable "region" {
+  description = "OCI Region"
+  type        = string
+  default     = "me-riyadh-1"
+}
+
 
